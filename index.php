8823ff0561f97fa4b376e17f75be6e2f0c106cf0
Mikael <mikael@MacBook-Pro-de-Mikael.local>
2021-07-07 02:26:20 -0300

    criando bloco das mais pedidas

--- index.php
+++ index.php
@@ -139,7 +139,8 @@
                         <a class="title-destaque-link" href="https://www.tecmundo.com.br/voxel/220543-novo-switch-chega-brasil-2022-switch-lite-em-setembro-r-1899.htm" title="Novo Switch chega ao Brasil em 2022 e Switch Lite em setembro por R$ 1899"> Novo Switch chega ao Brasil em 2022 e Switch Lite em setembro por R$ 1899 </a></h3></div>
             </article>
         </div>
-    </div>
+</div>
+<div class="corpo-broto">
     <div class="news-broto">  
         <article class="news-medium">
             <figure class="newsThumb">
@@ -204,9 +205,55 @@
             </div>
             </div>
             </div>
-        </article>
-       
+        </article>       
     </div>
+    <div class="widget-broto">
+        <div class="title-home d-flex justify-content-center">
+            <img class="title-home-icon " src="<?php bloginfo('template_url'); ?>/assets/img/icons/masPedidas.svg" class="d-block w-100" alt="...">
+            <h3>As Mais Mais da Broto</h3>
+            <i class="far fa-waveform"></i>
+        </div>
+        <hr/>
+            <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                01 - Gusttavo Lima - Café e Amor
+            </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+                <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingTwo">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Accordion Item #2
+            </button>
+            </h2>
+            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+                <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingThree">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                Accordion Item #3
+            </button>
+            </h2>
+            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
 </main>
 <?php
 get_footer();
